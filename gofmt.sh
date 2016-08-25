29a9472848ccb894371fd50b96422cf50339b6fc
Thor Prentow <prentow@cetrea.com>
2016-08-25 14:34:52 +0200

    Update gofmt script

--- gofmt.sh
+++ gofmt.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 set -v
-if find * ! -path *\vendor\* -type d -exec gofmt -d {} + | tee fmt.out | read; then { cat fmt.out; (exit 1) }; fi
+if find $GOPATH/src/$PACKAGE/* ! -path *\vendor\* -type d -exec gofmt -d {} + | tee fmt.out | read; then { cat fmt.out; (exit 1) }; fi
